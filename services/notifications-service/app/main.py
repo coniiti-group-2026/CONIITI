3bd0fa5191720ee6330ed374d6b6c14fb75d0ae9
kmcotame26 <karolmarcelacotame@gmail.com>
2026-03-31 20:02:05 -0500

    Correccion del main

--- services/notifications-service/app/main.py
+++ services/notifications-service/app/main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+# Modelo de datos
+class Notificacion(BaseModel):
+    correo: str
+    asunto: str
+    mensaje: str
+
 @app.get("/")
 def home():
-    return {"mensaje": "Servicio de notificaciones funcionando correctamente"}
+    return {"mensaje": "Servicio de notificaciones CONIITI activo"}
+
+@app.post("/notificar")
+def enviar_notificacion(notificacion: Notificacion):
+    return {
+        "estado": "enviado",
+        "correo": notificacion.correo,
+        "asunto": notificacion.asunto,
+        "mensaje": notificacion.mensaje
+    }
