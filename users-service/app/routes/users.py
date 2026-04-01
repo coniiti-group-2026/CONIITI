68ade1a6efb87da96511565ed1ea28de3ca2962d
Sergio-LTS <sdzuniga64@ucatolica.edu.co>
2026-03-31 22:47:53 -0500

    users service endpoints completos

--- users-service/app/routes/users.py
+++ users-service/app/routes/users.py
@@ -1,8 +1,8 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.database.connection import SessionLocal
 from app.models.user import User
-from app.schemas.user_schema import UserCreate, UserResponse
+from app.schemas.user_schema import UserCreate, UserResponse, UserUpdate
 
 router = APIRouter()
 
@@ -27,5 +27,44 @@ def create_user(user: UserCreate, db: Session = Depends(get_db)):
 
 @router.get("/")
 def get_users(db: Session = Depends(get_db)):
-    users = db.query(User).all()
-    return users
+    return db.query(User).all()
+
+@router.get("/{user_id}")
+def get_user(user_id: str, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.id == user_id).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
+
+@router.patch("/{user_id}")
+def update_user(user_id: str, user_data: UserUpdate, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.id == user_id).first()
+
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    if user_data.full_name:
+        user.full_name = user_data.full_name
+
+    if user_data.email:
+        user.email = user_data.email
+
+    if user_data.role:
+        user.role = user_data.role
+
+    db.commit()
+    db.refresh(user)
+
+    return user
+
+@router.delete("/{user_id}")
+def delete_user(user_id: str, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.id == user_id).first()
+
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    db.delete(user)
+    db.commit()
+
+    return {"message": "User deleted"}
